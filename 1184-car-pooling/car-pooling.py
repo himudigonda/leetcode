8ed19c51126ff3e9a119241a456293865abc663d
Himansh Mudigonda <himudigonda@gmail.com>
2025-06-14 10:29:41 -0700

    Time: 4 ms (34.90%) | Memory: 18 MB (99.24%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key = lambda t: t[1])
+        minheap = []
+        curpass = 0
+        for t in trips:
+            numpass, start, end = t
+            while minheap and minheap[0][0] <= start:
+                curpass -= minheap[-0][1]
+                heapq.heappop(minheap)
+
+            curpass += numpass
+            if curpass > capacity:
+                return False
+            heapq.heappush(minheap, [end, numpass])
+        return True
