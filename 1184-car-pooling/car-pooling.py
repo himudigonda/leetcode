14a369fd6cb12a2a6fcc6cc3f90b82757371de04
Himansh Mudigonda <himudigonda@gmail.com>
2025-06-14 10:33:14 -0700

    Time: 0 ms (100.00%) | Memory: 18.1 MB (75.25%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,12 +1,12 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips.sort(key = lambda t: t[1])
+        trips.sort(key=lambda t: t[1])
         minheap = []
         curpass = 0
         for t in trips:
             numpass, start, end = t
             while minheap and minheap[0][0] <= start:
-                curpass -= minheap[-0][1]
+                curpass -= minheap[0][1]
                 heapq.heappop(minheap)
 
             curpass += numpass
