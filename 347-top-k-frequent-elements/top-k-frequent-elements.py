c294ec93f9ea382f9314d528586162a7b9127d00
Himansh Mudigonda <himudigonda@gmail.com>
2025-06-25 06:19:50 -0700

    Time: 6 ms (56.76%) | Memory: 21.3 MB (61.60%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # # HashMap
+        # counts = Counter(nums)
+        # res = []
+        # freq = [[] for i in range(len(nums) + 1)]
+        # for key, count in counts.items():
+        #     freq[count].append(key)
+        # for i in range(len(freq) - 1, 0, -1):
+        #     for n in freq[i]:
+        #         res.append(n)
+        #         if len(res) == k:
+        #             return res
+
+        # # Heap
         counts = Counter(nums)
-        # sorted_counts = sorted(counts.items(), key=lambda item: item[1], reverse=True)
-        res = []
-        freq = [[] for i in range(len(nums) + 1)]
-        # freq = defaultdict(list)
+        heap = []
 
-        for key, count in counts.items():
-            freq[count].append(key)
-
-        # while len(res)<k:
-        #     res.append(freq[-1])
-        #     freq.pop()
-        # return res
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        for key, val in counts.items():
+            heapq.heappush(heap, (val, key))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return [keyval[1] for keyval in heap]
