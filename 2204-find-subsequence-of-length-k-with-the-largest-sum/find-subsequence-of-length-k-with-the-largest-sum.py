77134d71eb582ec4d1289322bd6bbb76d1134a52
Himansh Mudigonda <himudigonda@gmail.com>
2025-06-28 05:57:05 -0700

    Time: 7 ms (27.51%) | Memory: 18.2 MB (12.57%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
@@ -1,12 +1,26 @@
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        # (Number, Index) Pair
-        numsWithIndices = [(num, i) for i, num in enumerate(nums)]
+        # # # Sorting and Manipulation
+        # # (Number, Index) Pair
+        # numsWithIndices = [(num, i) for i, num in enumerate(nums)]
 
-        # Sort Asc wrt num -val
-        numsWithIndices.sort(key=lambda x: -x[0])
+        # # Sort Asc wrt num -val
+        # numsWithIndices.sort(key=lambda x: -x[0])
 
-        # sort Asc wrt index val until top K
-        topK = sorted(numsWithIndices[:k], key=lambda x: x[1])
+        # # sort Asc wrt index val until top K
+        # topK = sorted(numsWithIndices[:k], key=lambda x: x[1])
 
-        return [num for num, _ in topK]
+        # return [num for num, _ in topK]
+
+        # # # Heaps
+        arr = []
+        heapq.heapify(arr)
+        for i in range(len(nums)):
+            heapq.heappush(arr, [nums[i], i])
+            if len(arr) > k:
+                heapq.heappop(arr)
+        arr = sorted(arr, key=lambda d: d[1])
+        ans = []
+        for i in range(len(arr)):
+            ans.append(arr[i][0])
+        return ans
