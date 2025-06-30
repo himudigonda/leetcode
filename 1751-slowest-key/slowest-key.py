7fede4873effdc4ea9e4aa702e75f9e1716d5f58
Himansh Mudigonda <himudigonda@gmail.com>
2025-06-30 12:10:31 -0700

    Time: 2 ms (47.18%) | Memory: 17.8 MB (73.47%) - LeetSync

--- 1751-slowest-key/slowest-key.py
+++ 1751-slowest-key/slowest-key.py
@@ -1,16 +1,12 @@
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
-        if len(keysPressed) != len(releaseTimes):
-            return -1
-
         maxkey = keysPressed[0]
         maxduration = releaseTimes[0]
         for i in range(1, len(keysPressed)):
             curduration = releaseTimes[i] - releaseTimes[i - 1]
-            if maxduration < curduration:
+            if maxduration < curduration or (
+                maxduration == curduration and keysPressed[i] > maxkey
+            ):
                 maxduration = curduration
                 maxkey = keysPressed[i]
-            if maxduration == curduration:
-                if keysPressed[i] > maxkey:
-                    maxkey = keysPressed[i]
         return maxkey
