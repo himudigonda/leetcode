b816ea5585560f6817c5b48b6a519bdd39ade545
Himansh Mudigonda <himudigonda@gmail.com>
2025-07-31 17:53:52 -0700

    Time: 463 ms (33.01%) | Memory: 44.8 MB (50.24%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
@@ -1,12 +1,12 @@
 class Solution:
     def subarrayBitwiseORs(self, arr: List[int]) -> int:
         # # Iter1
-        # distinctSet = set()
-        # curSet = set()
-        # for num in arr:
-        #     curSet = {num | x for x in curSet} | {num}
-        #     distinctSet |= curSet
-        # return len(distinctSet)
+        distinctSet = set()
+        curSet = set()
+        for num in arr:
+            curSet = {num | x for x in curSet} | {num}
+            distinctSet |= curSet
+        return len(distinctSet)
 
         # # BruteForce
         # distinct_ors = set()
@@ -19,12 +19,12 @@ class Solution:
         # return len(distinct_ors)
 
         # Optimal
-        distinctSet = set()
-        curSet = set()
-        for x in arr:
-            newCurSet = {x}
-            for prevOr in curSet:
-                newCurSet.add(prevOr | x)
-            curSet = newCurSet
-            distinctSet.update(curSet)
-        return len(distinctSet)
+        # distinctSet = set()
+        # curSet = set()
+        # for x in arr:
+        #     newCurSet = {x}
+        #     for prevOr in curSet:
+        #         newCurSet.add(prevOr | x)
+        #     curSet = newCurSet
+        #     distinctSet.update(curSet)
+        # return len(distinctSet)
