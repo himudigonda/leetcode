d583b065899d579596fae1cc9745fb1e0b425f95
Himansh Mudigonda <himudigonda@gmail.com>
2025-08-01 18:03:43 -0700

    Time: 3 ms (37.22%) | Memory: 17.9 MB (33.84%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,29 +1,29 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         # # Iter 1
-        # stack = []
-        # cur = ""
-        # for c in path + "/":
-        #     if c == "/":
-        #         if cur == "..":
-        #             if stack:
-        #                 stack.pop()
-        #         elif cur != "" and cur != ".":
-        #             stack.append(cur)
-        #         cur = ""
-        #     else:
-        #         cur += c
-        # return "/" + "/".join(stack)
+        stack = []
+        cur = ""
+        for c in path + "/":
+            if c == "/":
+                if cur == "..":
+                    if stack:
+                        stack.pop()
+                elif cur != "" and cur != ".":
+                    stack.append(cur)
+                cur = ""
+            else:
+                cur += c
+        return "/" + "/".join(stack)
 
         # Iter 2
-        stack = []
-        for token in path.split("/"):
-            if token == "..":
-                if stack:
-                    stack.pop()
-            elif token and token != ".":
-                stack.append(token)
-        return "/" + "/".join(stack)
+        # stack = []
+        # for token in path.split("/"):
+        #     if token == "..":
+        #         if stack:
+        #             stack.pop()
+        #     elif token and token != ".":
+        #         stack.append(token)
+        # return "/" + "/".join(stack)
         # arr = [i for i in path.split('/')]
         # for i in arr:
         #     if i == '..':
