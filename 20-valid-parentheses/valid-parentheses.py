07cb52e464f96c1673f5dbbdb5d8a8289b8a77ae
Himansh Mudigonda <himudigonda@gmail.com>
2025-08-31 06:23:47 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (51.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,11 +3,18 @@ class Solution:
         stack = []
         mapping = {"}": "{", ")": "(", "]": "["}
 
+        # for i in s:
+        #     if i in mapping.values():
+        #         stack.append(i)
+        #     elif stack and len(stack) > 0 and i in mapping and stack[-1] == mapping[i]:
+        #         stack.pop()
+        #     else:
+        #         return False
+        # return True if not stack else False
+
         for i in s:
-            if i in mapping.values():
-                stack.append(i)
-            elif stack and len(stack) > 0 and i in mapping and stack[-1] == mapping[i]:
+            if stack and i in mapping and mapping[i] == stack[-1]:
                 stack.pop()
             else:
-                return False
+                stack.append(i)
         return True if not stack else False
