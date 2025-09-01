68e5f7d590bf8a9b24a894a745531c710e801edf
Himansh Mudigonda <himudigonda@gmail.com>
2025-09-01 07:51:45 -0500

    Time: 263 ms (37.10%) | Memory: 20.4 MB (43.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,33 +1,65 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # # BFS Optimal with visited()
+        # islands = 0
+        # visited = set()
+        # ROWS = len(grid)
+        # COLS = len(grid[0])
+
+        # def bfs(r, c):
+        #     q = deque()
+        #     visited.add((r, c))
+        #     q.append((r, c))
+
+        #     while q:
+        #         row, col = q.popleft()
+        #         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        #         for dr, dc in directions:
+        #             r, c = row + dr, col + dc
+        #             if (
+        #                 0 <= r < ROWS
+        #                 and 0 <= c < COLS
+        #                 and grid[r][c] == "1"
+        #                 and (r, c) not in visited
+        #             ):
+        #                 q.append((r, c))
+        #                 visited.add((r, c))
+
+        # for row in range(len(grid)):
+        #     for col in range(len(grid[0])):
+        #         if grid[row][col] == "1" and (row, col) not in visited:
+        #             islands += 1
+        #             bfs(row, col)
+        # return islands
+
+        if not grid:
+            return
+
         islands = 0
-        visited = set()
         ROWS = len(grid)
         COLS = len(grid[0])
 
-        def bfs(r, c):
-            q = deque()
-            visited.add((r, c))
-            q.append((r, c))
+        def dfs(row, col):
 
-            while q:
-                row, col = q.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            if (
+                row < 0
+                or col < 0
+                or row >= ROWS
+                or col >= COLS
+                or grid[row][col] != "1"
+            ):
+                return
 
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if (
-                        0 <= r < ROWS
-                        and 0 <= c < COLS
-                        and grid[r][c] == "1"
-                        and (r, c) not in visited
-                    ):
-                        q.append((r, c))
-                        visited.add((r, c))
+            grid[row][col] = "-1"
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for dr, dc in directions:
+                r, c = row + dr, col + dc
+                dfs(r, c)
 
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == "1" and (row, col) not in visited:
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1":
+                    dfs(i, j)
                     islands += 1
-                    bfs(row, col)
         return islands
