52608dfcbf81d9b4a41b8db45836549c57bcf99e
Himansh Mudigonda <himudigonda@gmail.com>
2025-11-28 11:35:31 -0600

    Time: 156 ms (57.94%) | Memory: 19.1 MB (38.80%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -12,13 +12,13 @@ class Solution:
         
         min_eating_speed = 1
         max_eating_speed = max_in_piles
-        best = max_in_piles
+        # best = max_in_piles
         while min_eating_speed <= max_eating_speed:
             speed = (min_eating_speed + max_eating_speed) // 2
             hours_to_eat_all = count_hours(speed)
             if hours_to_eat_all <= h:
-                best = min(speed, best)
+                # best = min(speed, best)
                 max_eating_speed = speed - 1
             else:
                 min_eating_speed = speed + 1
-        return best
+        return min_eating_speed
