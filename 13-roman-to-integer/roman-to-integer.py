b088fdd67b963a523477649c2e6e084a23f504ec
Himansh Mudigonda <himudigonda@gmail.com>
2025-12-22 15:32:51 -0700

    Time: 1 ms (87.33%) | Memory: 17.2 MB (99.55%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,11 +1,26 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        res = 0
-        mapping = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        # Base Implementation
+        # res = 0
+        # mapping = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-        for cur, nxt in zip(s, s[1:]):
-            if mapping[cur] < mapping[nxt]:
-                res -= mapping[cur]
+        # for cur, nxt in zip(s, s[1:]):
+        #     if mapping[cur] < mapping[nxt]:
+        #         res -= mapping[cur]
+        #     else:
+        #         res += mapping[cur]
+        # return res + mapping[s[-1]]
+
+        # Slightly Optimized Implementation
+        mapping = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        prev = 0
+        total = 0
+
+        for char in reversed(s):
+            cur = mapping[char]
+            if cur < prev:
+                total -= cur
             else:
-                res += mapping[cur]
-        return res + mapping[s[-1]]
+                total += cur
+            prev = cur
+        return total
