396b8dc5cffef0e2621fdc05937a8fe5c9e6c5c2
Himansh Mudigonda <himudigonda@gmail.com>
2025-12-26 13:30:18 -0700

    Time: 62 ms (31.11%) | Memory: 20.6 MB (93.91%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -10,10 +10,29 @@ class Solution:
     def lowestCommonAncestor(
         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
     ) -> "TreeNode":
-        while root:
-            if p.val > root.val and q.val > root.val:
-                root = root.right
-            elif p.val < root.val and q.val < root.val:
-                root = root.left
+        # # BST Method
+        # while root:
+        #     if p.val > root.val and q.val > root.val:
+        #         root = root.right
+        #     elif p.val < root.val and q.val < root.val:
+        #         root = root.left
+        #     else:
+        #         return root
+
+        # Recursion
+        if not root:
+            return root
+
+        def find(node):
+            if not node or node == p or node == q:
+                return node
+
+            left = find(node.left)
+            right = find(node.right)
+
+            if left and right:
+                return node
             else:
-                return root
+                return left or right
+
+        return find(root)
