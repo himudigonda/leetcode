d1f5fde43345f29ab62e3c823eb3ab4f27301ed1
Himansh Mudigonda <himudigonda@gmail.com>
2025-12-29 13:22:22 -0700

    Time: 0 ms (100.00%) | Memory: 17.3 MB (90.11%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -5,9 +5,13 @@ class Solution:
             if n == 1:
                 return True
             seen.add(n)
-            digits = list(str(n))
-            digits = [int(x) for x in digits]
-            for idx in range(len(digits)):
-                digits[idx] *= digits[idx]
-            n = sum(digits)
+            # digits = list(str(n))
+            # digits = [int(x) for x in digits]
+            # for idx in range(len(digits)):
+            #     digits[idx] *= digits[idx]
+            # n = sum(digits)
+            res = 0
+            for i in str(n):
+                res += int(i) ** 2
+            n = res
         return False
