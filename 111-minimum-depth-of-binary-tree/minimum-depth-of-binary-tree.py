fed5f7d69612cd597e8f693cb9c19ba76fb36a7d
Himansh Mudigonda <himudigonda@gmail.com>
2025-12-29 10:35:40 -0700

    Time: 3 ms (79.11%) | Memory: 43.8 MB (88.11%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -6,33 +6,33 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        # DFS
-        if not root:
-            return 0
-
-        if not root.left and not root.right:
-            return 1
-
-        min_depth = float("inf")
-
-        if root.left:
-            min_depth = min(min_depth, self.minDepth(root.left))
-        if root.right:
-            min_depth = min(min_depth, self.minDepth(root.right))
-
-        return 1 + min_depth
-
-        # BFS
+        # # DFS
         # if not root:
         #     return 0
-        # q = deque([(root, 1)])
 
-        # while q:
-        #     node, depth = q.popleft()
-        #     if not node.left and not node.right:
-        #         return depth
+        # if not root.left and not root.right:
+        #     return 1
 
-        #     if node.left:
-        #         q.append((node.left, depth + 1))
-        #     if node.right:
-        #         q.append((node.right, depth + 1))
+        # min_depth = float("inf")
+
+        # if root.left:
+        #     min_depth = min(min_depth, self.minDepth(root.left))
+        # if root.right:
+        #     min_depth = min(min_depth, self.minDepth(root.right))
+
+        # return 1 + min_depth
+
+        # BFS
+        if not root:
+            return 0
+        q = deque([(root, 1)])
+
+        while q:
+            node, depth = q.popleft()
+            if not node.left and not node.right:
+                return depth
+
+            if node.left:
+                q.append((node.left, depth + 1))
+            if node.right:
+                q.append((node.right, depth + 1))
