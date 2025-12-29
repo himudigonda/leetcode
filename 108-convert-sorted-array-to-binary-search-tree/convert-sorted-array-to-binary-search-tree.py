53d2f3ca8274280a22c2f9620dc0876900a617c7
Himansh Mudigonda <himudigonda@gmail.com>
2025-12-29 10:18:51 -0700

    Time: 2 ms (79.49%) | Memory: 18.7 MB (86.00%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -6,11 +6,25 @@
 #         self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        if not nums:
-            return None
+        # # Pass arr as args to self recursion
+        # if not nums:
+        #     return None
 
-        mid = len(nums) // 2
-        root = TreeNode(nums[mid])
-        root.left = self.sortedArrayToBST(nums[:mid])
-        root.right = self.sortedArrayToBST(nums[mid + 1 :])
-        return root
+        # mid = len(nums) // 2
+        # root = TreeNode(nums[mid])
+        # root.left = self.sortedArrayToBST(nums[:mid])
+        # root.right = self.sortedArrayToBST(nums[mid + 1 :])
+        # return root
+
+        # helper function based recursion with left and right
+        def sortitup(left, right):
+            if left > right:
+                return
+
+            mid = (left + right) // 2
+            node = TreeNode(nums[mid])
+            node.left = sortitup(left, mid - 1)
+            node.right = sortitup(mid + 1, right)
+            return node
+
+        return sortitup(0, len(nums) - 1)
