8aed375436483c81c8914f0f5327e678136e7f7c
Himansh Mudigonda <himudigonda@gmail.com>
2025-12-30 16:33:57 -0700

    Time: 0 ms (100.00%) | Memory: 17.5 MB (85.61%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-stone for stone in stones]
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            stone1 = heapq.heappop(stones)
+            stone2 = heapq.heappop(stones)
+            if stone1 == stone2:
+                continue
+            else:
+                heapq.heappush(stones, -abs(stone1 - stone2))
+        return -stones[0] if stones else 0
