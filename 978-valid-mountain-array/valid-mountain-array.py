97abd3e7c3d32a112179e88853105539c29125e0
Himansh Mudigonda <himudigonda@gmail.com>
2025-12-31 16:07:55 -0700

    Time: 164 ms (47.42%) | Memory: 18.8 MB (78.31%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.py
+++ 978-valid-mountain-array/valid-mountain-array.py
@@ -4,17 +4,25 @@ class Solution:
         if n < 3:
             return False
 
-        peak = arr.index(max(arr))
+        # peak = arr.index(max(arr))
 
-        if peak == 0 or peak == n - 1:
+        # if peak == 0 or peak == n - 1:
+        #     return False
+
+        # for i in range(peak):
+        #     if arr[i] >= arr[i + 1]:
+        #         return False
+
+        # for i in range(peak, n - 1):
+        #     if arr[i] <= arr[i + 1]:
+        #         return False
+
+        # return True
+        i = 0
+        while i < n - 1 and arr[i] < arr[i + 1]:
+            i += 1
+        if i == 0 or i == n - 1:
             return False
-
-        for i in range(peak):
-            if arr[i] >= arr[i + 1]:
-                return False
-
-        for i in range(peak, n - 1):
-            if arr[i] <= arr[i + 1]:
-                return False
-
-        return True
+        while i < n - 1 and arr[i] > arr[i + 1]:
+            i += 1
+        return i == n - 1
