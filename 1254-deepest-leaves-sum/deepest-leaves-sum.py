54e9849385717df537cacf5ab1287534bc627344
Himansh Mudigonda <himudigonda@gmail.com>
2026-01-03 17:46:08 -0700

    Time: 19 ms (44.41%) | Memory: 20.5 MB (61.42%) - LeetSync

--- 1254-deepest-leaves-sum/deepest-leaves-sum.py
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.py
@@ -6,18 +6,34 @@
 #         self.right = right
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
-        mapping = defaultdict(int)
-        maxdepth = 0
+        # # DFS
+        # mapping = defaultdict(int)
+        # maxdepth = 0
 
-        def dfs(node, depth):
-            nonlocal maxdepth
-            if not node: return
-            
-            maxdepth = max(maxdepth, depth)
-            mapping[depth] += node.val
-            dfs(node.left, depth + 1)
-            dfs(node.right, depth + 1)
-            return
+        # def dfs(node, depth):
+        #     nonlocal maxdepth
+        #     if not node: return
 
-        dfs(root, 0)
-        return mapping[maxdepth]
+        #     maxdepth = max(maxdepth, depth)
+        #     mapping[depth] += node.val
+        #     dfs(node.left, depth + 1)
+        #     dfs(node.right, depth + 1)
+        #     return
+
+        # dfs(root, 0)
+        # return mapping[maxdepth]
+
+        # BFS
+        q = deque([root])
+
+        while q:
+            level_sum = 0
+            for i in range(len(q)):
+                node = q.popleft()
+                level_sum += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if not q:
+                return level_sum
