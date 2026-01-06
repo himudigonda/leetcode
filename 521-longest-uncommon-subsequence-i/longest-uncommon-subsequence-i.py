7f1cfe93048bec307662ce17d125505b9901974e
Himansh Mudigonda <himudigonda@gmail.com>
2026-01-06 15:40:29 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.58%) - LeetSync

--- 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.py
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.py
@@ -1,18 +1,22 @@
 class Solution:
     def findLUSlength(self, a: str, b: str) -> int:
+        # if a == b:
+        #     return -1
+        # countsA = Counter(a)
+        # countsB = Counter(b)
+        # resA = 0
+        # resB = 0
+
+        # for key, val in countsA.items():
+        #     if key not in set(countsB.items()):
+        #         resA += val
+
+        # for key, val in countsB.items():
+        #     if key not in set(countsB.items()):
+        #         resB += val
+
+        # return max(resA, resB) if resA and resB else -1
+
         if a == b:
             return -1
-        countsA = Counter(a)
-        countsB = Counter(b)
-        resA = 0
-        resB = 0
-
-        for key, val in countsA.items():
-            if key not in set(countsB.items()):
-                resA += val
-
-        for key, val in countsB.items():
-            if key not in set(countsB.items()):
-                resB += val
-
-        return max(resA, resB) if resA and resB else -1
+        return max(len(a), len(b))
