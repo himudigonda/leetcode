772051eba91d99d4e183c17a54cc6f1ad199d3d0
Himansh Mudigonda <himudigonda@gmail.com>
2026-01-09 16:22:41 -0700

    Time: 0 ms (100.00%) | Memory: 19.6 MB (5.14%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.py
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.py
@@ -6,27 +6,42 @@
 #         self.right = right
 class Solution:
     def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return None
+        # if not root:
+        #     return None
 
-        depths = defaultdict(int)
-        parent = defaultdict(TreeNode)
-        q = deque([(root, 0)])
-        maxdepth = -1
+        # depths = defaultdict(int)
+        # parent = defaultdict(TreeNode)
+        # q = deque([(root, 0)])
+        # maxdepth = -1
 
-        while q:
-            node, depth = q.popleft()
-            depths[node] = depth
-            maxdepth = max(maxdepth, depth)
-            if node.left:
-                q.append((node.left, depth + 1))
-                parent[node.left] = node
-            if node.right:
-                q.append((node.right, depth + 1))
-                parent[node.right] = node
+        # while q:
+        #     node, depth = q.popleft()
+        #     depths[node] = depth
+        #     maxdepth = max(maxdepth, depth)
+        #     if node.left:
+        #         q.append((node.left, depth + 1))
+        #         parent[node.left] = node
+        #     if node.right:
+        #         q.append((node.right, depth + 1))
+        #         parent[node.right] = node
 
-        maxes = [key for key, val in depths.items() if val == maxdepth]
+        # maxes = [key for key, val in depths.items() if val == maxdepth]
 
-        while len(maxes) > 1:
-            maxes = {parent[node] for node in maxes}
-        return list(maxes)[0]
+        # while len(maxes) > 1:
+        #     maxes = {parent[node] for node in maxes}
+        # return list(maxes)[0]
+
+        def dfs(node):
+            if not node:
+                return 0, None
+
+            left_depth, left_node = dfs(node.left)
+            right_depth, right_node = dfs(node.right)
+
+            if left_depth > right_depth:
+                return left_depth + 1, left_node
+            if right_depth > left_depth:
+                return right_depth + 1, right_node
+            return left_depth + 1, node
+
+        return dfs(root)[1]
