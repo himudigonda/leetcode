330027cb722c40c0fe9f9276c907528c904790f3
Himansh Mudigonda <himudigonda@gmail.com>
2026-01-13 01:52:46 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.66%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -4,6 +4,5 @@ class Solution:
         for i in range(1, len(points)):
             x = abs(points[i][0] - points[i - 1][0])
             y = abs(points[i][1] - points[i - 1][1])
-            print(x, y)
             res += max(x, y)
         return res
