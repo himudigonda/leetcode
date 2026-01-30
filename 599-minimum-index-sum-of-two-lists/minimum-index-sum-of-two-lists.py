a0cfd7871d333a3da5f0fcfccedecfa50b02efe3
Himansh Mudigonda <himudigonda@gmail.com>
2026-01-30 16:17:25 -0700

    Time: 5 ms (80.15%) | Memory: 19.9 MB (13.02%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,24 +1,43 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        set1 = set(list1)
-        set2 = set(list2)
-        common = set1.intersection(set2)
-        resnum = float("inf")
-        res = []
-        map1 = defaultdict(int)
-        map2 = defaultdict(int)
-        for idx, val in enumerate(list1):
-            map1[val] = idx
-        for idx, val in enumerate(list2):
-            map2[val] = idx
+        # set1 = set(list1)
+        # set2 = set(list2)
+        # common = set1.intersection(set2)
+        # resnum = float("inf")
+        # res = []
+        # map1 = defaultdict(int)
+        # map2 = defaultdict(int)
+        # for idx, val in enumerate(list1):
+        #     map1[val] = idx
+        # for idx, val in enumerate(list2):
+        #     map2[val] = idx
 
-        for i in common:
-            l1 = map1[i]
-            l2 = map2[i]
-            summ = l1 + l2
-            if summ < resnum:
-                resnum = summ
-                res = [i]
-            elif summ == resnum:
-                res.append(i)
-        return res
+        # for i in common:
+        #     l1 = map1[i]
+        #     l2 = map2[i]
+        #     summ = l1 + l2
+        #     if summ < resnum:
+        #         resnum = summ
+        #         res = [i]
+        #     elif summ == resnum:
+        #         res.append(i)
+        # return res
+
+        idx1 = defaultdict(int)
+        for idx, val in enumerate(list1):
+            idx1[val] = idx
+
+        least = len(list1) + len(list2)
+        common = []
+        for j, val in enumerate(list2):
+            if val not in idx1:
+                continue
+            i = idx1[val]
+            if i + j > least:
+                continue
+            elif i + j == least:
+                common.append(val)
+            else:
+                least = i + j
+                common = [val]
+        return common
