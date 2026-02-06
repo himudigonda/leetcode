e3b8915f276a552319f7049c21de0489fd896b84
Himansh Mudigonda <himudigonda@gmail.com>
2026-02-06 12:58:28 -0700

    Time: 35 ms (49.07%) | Memory: 21.8 MB (53.17%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # if k == 1: return 1
+        # if len(nums) == 1: return 0
+        # left = 0
+        # prefix = [0]
+        # for idx, val in enumerate(nums):
+        #     prefix.append(prefix[-1] + val)
+        # count = 0
+        # for right in range(1, len(prefix)):
+        #     if prefix[right] - prefix[left] <= k:
+        #         count += 1
+        #     else:
+        #         left += 1
+        # return count
+
+        mapping = {0: 1}
+        cursum = 0
+        count = 0
+
+        for num in nums:
+            cursum += num
+            diff = cursum - k
+            if diff in mapping:
+                count += mapping[diff]
+            mapping[cursum] = mapping.get(cursum, 0) + 1
+        return count
