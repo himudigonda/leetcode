01786edf83a57128bb0fed8935fa914cf0bab04f
Himansh Mudigonda <himudigonda@gmail.com>
2026-02-05 17:40:44 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.94%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
@@ -3,11 +3,8 @@ class Solution:
         count = 0
 
         odds_in_n = (n + 1) // 2
-        even_in_m = m // 2
-        count += odds_in_n * even_in_m
-
-        even_in_n = n // 2
         odds_in_m = (m + 1) // 2
-        count += even_in_n * odds_in_m
+        even_in_m = m // 2
+        even_in_n = n // 2
 
-        return count
+        return (odds_in_n * even_in_m) + (even_in_n * odds_in_m)
