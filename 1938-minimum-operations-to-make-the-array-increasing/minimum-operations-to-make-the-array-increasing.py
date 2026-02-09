0d1428d8982b8a0f0f7fa783a031121e02b1911f
Himansh Mudigonda <himudigonda@gmail.com>
2026-02-09 14:19:41 -0700

    Time: 103 ms (75.99%) | Memory: 19.9 MB (55.49%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
@@ -1,10 +1,25 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        curmax = float("-inf")
         count = 0
+        prev = nums[0]
 
         for idx in range(1, len(nums)):
-            if nums[idx - 1] >= nums[idx]:
-                count += nums[idx - 1] - nums[idx] + 1
-                nums[idx] = nums[idx - 1] + 1
+            if prev >= nums[idx]:
+                temp = prev - nums[idx] + 1
+                count += temp
+                prev += 1
+            else:
+                prev = nums[idx]
         return count
+
+        # res = 0
+        # prev = nums[0]
+
+        # for num in nums[1:]:
+        #     if prev < num:
+        #         prev = num
+        #         continue
+
+        #     res += prev + 1 - num
+        #     prev = prev + 1
+        # return res
