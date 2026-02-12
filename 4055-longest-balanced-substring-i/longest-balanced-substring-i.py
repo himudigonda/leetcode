40b9338065e9a7034caae70904f34d5dd6ce5629
Himansh Mudigonda <himudigonda@gmail.com>
2026-02-12 15:38:22 -0700

    Time: 1091 ms (94.19%) | Memory: 19.4 MB (22.02%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.py
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.py
@@ -2,18 +2,17 @@ class Solution:
     def longestBalanced(self, s: str) -> int:
         n = len(s)
         ans = 0
-
+        s = [ord(ch) - ord('a') for ch in s]
         for i in range(n):
             freq = [0] * 26
             distinct = 0
             maxFreq = 0
 
             for j in range(i, n):
-                idx = ord(s[j]) - ord('a')
-                if freq[idx] == 0:
+                if freq[s[j]] == 0:
                     distinct += 1
-                freq[idx] += 1
-                maxFreq = max(maxFreq, freq[idx])
+                freq[s[j]] += 1
+                maxFreq = max(maxFreq, freq[s[j]])
                 length = j - i + 1
                 if length == distinct * maxFreq:
                     ans = max(ans, length)
