4af67ee7cb3044e7ec6a70bf57970446f4e59b28
Himansh Mudigonda <himudigonda@gmail.com>
2026-02-22 11:31:27 -0700

    Time: 7 ms (90.32%) | Memory: 19.4 MB (62.39%) - LeetSync

--- 4224-find-the-score-difference-in-a-game/find-the-score-difference-in-a-game.py
+++ 4224-find-the-score-difference-in-a-game/find-the-score-difference-in-a-game.py
@@ -1,17 +1,33 @@
 class Solution:
     def scoreDifference(self, nums: List[int]) -> int:
-        player1 = 0
-        player2 = 0
-        player1flag = True
-        player2flag = False
+        # player1 = 0
+        # player2 = 0
+        # player1flag = True
+        # player2flag = False
+
+        # for idx, val in enumerate(nums):
+        #     if idx % 6 == 5:
+        #         player1flag = not player1flag
+        #         player2flag = not player2flag
+        #     if val % 2 == 1:
+        #         player1flag = not player1flag
+        #         player2flag = not player2flag
+        #     player1 += val if player1flag else 0
+        #     player2 += val if player2flag else 0
+        # return player1 - player2
+
+        scores = [0, 0]
+        active = True
 
         for idx, val in enumerate(nums):
+            if val % 2:
+                active = not active
             if idx % 6 == 5:
-                player1flag = not player1flag
-                player2flag = not player2flag
-            if val % 2 == 1:
-                player1flag = not player1flag
-                player2flag = not player2flag
-            player1 += val if player1flag else 0
-            player2 += val if player2flag else 0
-        return player1 - player2
+                active = not active
+
+            if active:
+                scores[0] += val
+            else:
+                scores[1] += val
+
+        return scores[0] - scores[1]
