1f280591c1f3a5362cdad4dfd0ab12e5541212b7
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-04 13:11:30 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.32%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -2,19 +2,28 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         left = 0
         right = len(nums) - 1
-        best = nums[0]
+        # best = nums[0]
 
-        while left <= right:
+        # while left <= right:
+        #     mid = (left + right) // 2
+        #     best = min(best, nums[mid])
+        #     if nums[mid] < nums[right] and nums[mid] >= nums[left]:
+        #         right = mid - 1
+        #     elif nums[mid] > nums[right] and nums[mid] >= nums[left]:
+        #         left = mid + 1
+        #     elif nums[mid] < nums[right] and nums[mid] <= nums[left]:
+        #         right = mid - 1
+        #     else:
+        #         print(nums[left], nums[right], nums[mid], left, right, mid)
+        #         best = min(best, nums[mid], nums[left], nums[right])
+        #         return best
+        # return best
+
+        while left < right:
             mid = (left + right) // 2
-            best = min(best, nums[mid])
-            if nums[mid] < nums[right] and nums[mid] >= nums[left]:
-                right = mid - 1
-            elif nums[mid] > nums[right] and nums[mid] >= nums[left]:
+
+            if nums[mid] > nums[right]:
                 left = mid + 1
-            elif nums[mid] < nums[right] and nums[mid] <= nums[left]:
-                right = mid - 1
             else:
-                print(nums[left], nums[right], nums[mid], left, right, mid)
-                best = min(best, nums[mid], nums[left], nums[right])
-                return best
-        return best
+                right = mid
+        return nums[left]
