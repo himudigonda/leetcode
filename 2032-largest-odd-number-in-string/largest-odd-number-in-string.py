e1ec9d32b2e760071af78f869dd4dda0ad1c3c1c
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-06 17:32:39 -0700

    Time: 3 ms (96.14%) | Memory: 20 MB (97.64%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -4,4 +4,4 @@ class Solution:
         for right in range(len(num) - 1, -1, -1):
             if int(num[right]) % 2 == 1:
                 return num[:right + 1]
-            if right == 0: return ""
+        return ""
