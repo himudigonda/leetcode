4e605fa28bb7d062a6cd34b3f4a549a4f09b563d
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-07 17:32:22 -0700

    Time: 553 ms (15.91%) | Memory: 20 MB (84.88%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -3,27 +3,36 @@ class Solution:
         n = len(s)
         s = s * 2
 
-        target1 = ""
-        target2 = ""
+        # target1 = ""
+        # target2 = ""
 
-        for i in range(len(s)):
-            target1 += "0" if i % 2 == 0 else "1"
-            target2 += "1" if i % 2 == 0 else "0"
+        # for i in range(len(s)):
+        #     target1 += "0" if i % 2 == 0 else "1"
+        #     target2 += "1" if i % 2 == 0 else "0"
 
         diff1, diff2 = 0, 0
         minFlips = float("inf")
         left = 0
 
         for right in range(len(s)):
-            if s[right] != target1[right]:
+            # if s[right] != target1[right]:
+            #     diff1 += 1
+            # if s[right] != target2[right]:
+            #     diff2 += 1
+            if int(s[right]) != (right % 2):
                 diff1 += 1
-            if s[right] != target2[right]:
+            if int(s[right]) != (1 - (right % 2)):
                 diff2 += 1
 
             if (right - left + 1) > n:
-                if s[left] != target1[left]:
+                # if s[left] != target1[left]:
+                #     diff1 -= 1
+                # if s[left] != target2[left]:
+                #     diff2 -= 1
+                # left += 1
+                if int(s[left]) != (left % 2):
                     diff1 -= 1
-                if s[left] != target2[left]:
+                if int(s[left]) != (1 - (left % 2)):
                     diff2 -= 1
                 left += 1
 
