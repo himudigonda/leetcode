d1d77d8c706ae5e5f7626c00837aa0b1d8ca2558
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-13 11:37:42 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (61.28%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -5,19 +5,16 @@ class Solution:
         """
         ptr = len(nums) - 2
         while ptr >= 0 and nums[ptr] >= nums[ptr + 1]:
-                ptr -= 1
-
-
+            ptr -= 1
 
         if ptr >= 0:
             target_finder = len(nums) - 1
-        # while nums[target_finder] <=
+            # while nums[target_finder] <=
             while nums[target_finder] <= nums[ptr]:
                 target_finder -= 1
             nums[target_finder], nums[ptr] = nums[ptr], nums[target_finder]
-        
-    
-        end = len(nums) -1 
+
+        end = len(nums) - 1
         ptr += 1
         while ptr < end:
             nums[ptr], nums[end] = nums[end], nums[ptr]
@@ -25,7 +22,6 @@ class Solution:
             end -= 1
         return nums
 
-        
         # nums[ptr], nums[ptr + 1] = nums[ptr + 1], nums[ptr]
         # nums[ptr + 2 :] = sorted(nums[ptr + 2 :])
         # return nums
