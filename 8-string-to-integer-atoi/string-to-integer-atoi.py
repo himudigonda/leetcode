0ba544b4ba7e5a87f6376d4260ca172f8051ab5d
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-13 13:20:14 -0700

    Time: 0 ms (100.00%) | Memory: 19.9 MB (11.89%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,6 +1,9 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        res = []
+        res = 0
+        INT_MIN = -(2**31)  # -2147483648
+        INT_MAX = 2**31 - 1  # 2147483647
+        # res = []
         s = s.strip()
         if not s:
             return 0
@@ -10,23 +13,29 @@ class Solution:
         elif s[0] == "+":
             neg = 1
             s = s[1:]
-        
+
         else:
             neg = 1
 
         def rec(idx):
+            nonlocal res
             if idx >= len(s):
                 return
             if s[idx].isdigit():
-                res.append(s[idx])
+                # res.append(s[idx])
+                res = res * 10 + int(s[idx])
+                if neg * res > INT_MAX:
+                    return
+                if neg * res < INT_MIN:
+                    return
+
             else:
                 # if res: return
                 return
             rec(idx + 1)
 
         rec(0)
-        INT_MIN = -(2**31)  # -2147483648
-        INT_MAX = 2**31 - 1  # 2147483647
-        res = int("".join(res)) if res else 0
+        # res = int("".join(res)) if res else 0
+        res = res
         res = max(INT_MIN, min(INT_MAX, neg * res))
         return res
