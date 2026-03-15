cf2991a753c27fabfb04ff1fec2ce24a5d900a5f
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-15 15:19:30 -0700

    Time: 2 ms (0.00%) | Memory: 19.2 MB (100.00%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
@@ -1,14 +1,22 @@
 class Solution:
     def countCommas(self, n: int) -> int:
         # blocks = math.floor(math.log(n, 10))
-        blocks = len(str(n)) - 1
-        
-        if blocks < 3:
-            return 0
+        # blocks = len(str(n)) - 1
 
-        groups = blocks // 3
+        # if blocks < 3:
+        #     return 0
+
+        # groups = blocks // 3
+        # res = 0
+        # for i in range(1, groups):
+        #     res += (10 ** (3 * (i + 1)) - 10 ** (3 * i)) * i
+        # res += groups * (n - 10 ** (groups * 3) + 1)
+        # return res
+
         res = 0
-        for i in range(1, groups):
-            res += (10 ** (3 * (i + 1)) - 10 ** (3 * i)) * i
-        res += groups * (n - 10 ** (groups * 3) + 1)
+        step = 1000
+
+        while n >= step:
+            res += n - step + 1
+            step *= 1000
         return res
