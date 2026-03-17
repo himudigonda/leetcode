0b41e23f732f22f5f75353ab2c34ec9d3b94188b
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-17 11:39:27 -0700

    Time: 128 ms (60.21%) | Memory: 36.4 MB (63.44%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.py
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.py
@@ -1,16 +1,34 @@
 class Solution:
     def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        # ROWS = len(matrix)
+        # COLS = len(matrix[0])
+        # height = [0] * COLS
+        # ans = 0
+
+        # for i in range(ROWS):
+        #     for j in range(COLS):
+        #         if matrix[i][j] == 1:
+        #             height[j] += 1
+        #         else:
+        #             height[j] = 0
+
+        #     cheight = sorted(height, reverse=True)
+        #     for j in range(COLS):
+        #         ans = max(ans, (j + 1) * cheight[j])
+        # return ans
         ROWS = len(matrix)
         COLS = len(matrix[0])
-
-        for i in range(1, ROWS):
-            for j in range(COLS):
-                if matrix[i][j] == 1:
-                    matrix[i][j] += matrix[i - 1][j]
-
+        prev = [0] * COLS
         ans = 0
+
         for i in range(ROWS):
-            matrix[i].sort(reverse=True)
+            cur = matrix[i]
             for j in range(COLS):
-                ans = max(ans, (j + 1) * matrix[i][j])
+                if cur[j] == 1:
+                    cur[j] += prev[j]
+
+            ccur = sorted(cur, reverse=True)
+            for j in range(COLS):
+                ans = max(ans, (j + 1) * ccur[j])
+            prev = cur
         return ans
