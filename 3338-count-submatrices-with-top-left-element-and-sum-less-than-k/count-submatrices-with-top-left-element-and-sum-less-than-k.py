0a5c3f2f57a3d863d283a9a9311dcfa932ebe8dc
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-18 16:31:10 -0700

    Time: 115 ms (96.77%) | Memory: 54.8 MB (99.35%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -4,16 +4,21 @@ class Solution:
         COLS = len(grid[0])
         res = 0
 
-        temp = [[0] * (COLS + 1) for i in range(ROWS + 1)]
+        # temp = [[0] * (COLS + 1) for i in range(ROWS + 1)]
+        temp = [0] * COLS
 
         for i in range(ROWS):
+            prefix_row = 0
             for j in range(COLS):
-                temp[i + 1][j + 1] = (
-                    grid[i][j] + temp[i][j + 1] + temp[i + 1][j] - temp[i][j]
-                )
-                if temp[i + 1][j + 1] <= k:
+
+                # temp[i + 1][j + 1] = (
+                #     grid[i][j] + temp[i][j + 1] + temp[i + 1][j] - temp[i][j]
+                # )
+                prefix_row += grid[i][j]
+                temp[j] += prefix_row
+                # if temp[i + 1][j + 1] <= k:
+                if temp[j] <= k:
                     res += 1
                 else:
                     break
         return res
-        
