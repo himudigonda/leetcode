eb0e4425f3e73d1885fb2775d6de58f3afe0ab49
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-20 16:56:42 -0700

    Time: 3 ms (76.10%) | Memory: 19.4 MB (66.72%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -0,0 +1,34 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # bfs
+        q = deque()
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        oranges = 0
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 2:
+                    q.append((i, j))
+                if grid[i][j] == 1:
+                    oranges += 1
+        if oranges == 0:
+            return 0
+        if not q:
+            return -1
+
+        time = 0
+        dirs = [[0, -1], [0, 1], [1, 0], [-1, 0]]
+        visited = grid
+        while q and oranges > 0:
+            qlen = len(q)
+            for qidx in range(qlen):
+                i, j = q.popleft()
+                for dr, dc in dirs:
+                    ni, nj = i + dr, j + dc
+                    if 0 <= ni < ROWS and 0 <= nj < COLS and visited[ni][nj] == 1:
+                        visited[ni][nj] = 2
+                        oranges -= 1
+                        q.append((ni, nj))
+            time += 1
+        return time if oranges == 0 else -1
