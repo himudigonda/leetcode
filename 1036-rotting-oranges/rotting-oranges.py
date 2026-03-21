1f1a95f05eb31afc336d07c4bab781fd24460c07
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-20 17:00:33 -0700

    Time: 4 ms (36.00%) | Memory: 19.3 MB (66.72%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -9,7 +9,7 @@ class Solution:
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 2:
-                    q.append((i, j))
+                    q.append((i, j, 0))
                 if grid[i][j] == 1:
                     oranges += 1
         if oranges == 0:
@@ -19,16 +19,24 @@ class Solution:
 
         time = 0
         dirs = [[0, -1], [0, 1], [1, 0], [-1, 0]]
-        visited = grid
         while q and oranges > 0:
-            qlen = len(q)
-            for qidx in range(qlen):
-                i, j = q.popleft()
-                for dr, dc in dirs:
-                    ni, nj = i + dr, j + dc
-                    if 0 <= ni < ROWS and 0 <= nj < COLS and visited[ni][nj] == 1:
-                        visited[ni][nj] = 2
-                        oranges -= 1
-                        q.append((ni, nj))
-            time += 1
+            # qlen = len(q)
+            # for qidx in range(qlen):
+            #     i, j, t = q.popleft()
+            #     for dr, dc in dirs:
+            #         ni, nj = i + dr, j + dc
+            #         if 0 <= ni < ROWS and 0 <= nj < COLS and grid[ni][nj] == 1:
+            #             grid[ni][nj] = 2
+            #             oranges -= 1
+            #             q.append((ni, nj))
+            # time += 1
+            i, j, t = q.popleft()
+            for dr, dc in dirs:
+                ni, nj = i + dr, j + dc
+                if 0 <= ni < ROWS and 0 <= nj < COLS and grid[ni][nj] == 1:
+                    grid[ni][nj] = 2
+                    time = t + 1
+                    q.append((ni, nj, time))
+                    oranges -= 1
+        # time += 1
         return time if oranges == 0 else -1
