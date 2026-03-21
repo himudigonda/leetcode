98b5a4ef98ecb082e6159a99d8accfcb34faeb29
Himansh Mudigonda <himudigonda@gmail.com>
2026-03-20 17:28:22 -0700

    Time: 11 ms (19.87%) | Memory: 19.5 MB (71.33%) - LeetSync

---
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        mapping = []
+        for num in arr:
+            cur = bin(num)[2:]
+            counts = 0
+            for i in cur:
+                if i == "1":
+                    counts += 1
+            mapping.append((num, counts))
+
+        temp = sorted(mapping, key=lambda x: (x[1], x[0]))
+        res = [i for i, j in temp]
+        return res
